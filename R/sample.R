9a9d5d7f14d9d573822bec1d628a0d4ae68453fb
Ralf Stubner <ralf.stubner@daqana.com>
2018-09-27 20:12:09 +0200

    Add R Core's copyright

--- R/sample.R
+++ R/sample.R
@@ -1,3 +1,23 @@
+#  This file is based on src/library/base/R/sample.R
+#  Part of the R package, https://www.R-project.org
+#
+#  Copyright (C) 1995-2016 The R Core Team
+#  Copyright (C) 2018 Ralf Stubner (daqana GmbH)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  https://www.R-project.org/Licenses/
+
+
 ##' @title Unbiased Random Samples and Permutations
 ##' @description These functions provide an unbiased alternative to the corresponding
 ##'   \code{base} functions.
